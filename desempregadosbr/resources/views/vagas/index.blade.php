cb20a3f33acad60045265df2f4e493625505fbce
Lucas <Lucaselorenzohenrique@gmail.com>
2022-07-07 11:52:38 -0300

    View index

---
+++ desempregadosbr/resources/views/vagas/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layout.layout')
+
+@section('content')
+
+    <!--- Formulario com input search --->
+    <header>
+        <div class="container d-flex justify-content-center">
+            <form class="mt-4 w-50" action="">
+                <div class="d-flex">
+                    <button class="bg-success text-white px-3 py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </button>
+                    <input class="form-control d-inline me-2 border border-left-0" type="search" placeholder="Search" aria-label="Search">
+                </div>
+            </form>
+        </div>
+        <div class="container d-flex justify-content-center mt-3">
+            <p class="text-success">Explore vagas de trabalhos com simplicidade e sem burocracia !</p>
+        </div>
+    </header>
+    <!--- --->
+    
+    <!--- Lista de vagas --->
+    <main>
+        <div class="container">
+        @if($vagas != null)
+        @foreach($vagas as $vaga)
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        @endforeach
+        @endif
+        </div>
+    </main>
+    <!--- --->
+
+
+    <!-- Modal login -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="container">
+                    <div class="modal-header d-flex justify-content-end">
+                        <a href="/" class="btn btn-sm bg-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="modal-body">
+                        <h4><strong>Faça login ou registre-se em segundos</strong></h4>
+                        <p>Use seu email ou outra conta para entrar.</p>
+                        <div class="d-flex justify-content-center my-2">
+                            <form action="" style="width: 60%;" class="mt-4">
+                                <button class="btn-redes btn btn-sm btn-white border border-dark p-2 w-100 d-flex justify-content-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-google align-self-center" viewBox="0 0 16 16">
+                                        <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+                                    </svg>
+                                    <p class="align-self-center mx-3 mb-0">Continuar com o Google</p>
+                                </button>
+                                <button class="btn-redes btn btn-white border border-dark btn-sm mt-2 p-2 w-100 d-flex justify-content-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                    </svg>  
+                                    <p class="align-self-center mx-3 mb-0">Continuar com o facebook</p>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="container mt-5">
+                            <p>Ao continuar você concorda com nossos Termos de uso. Leia nossa Política de privacidade</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
