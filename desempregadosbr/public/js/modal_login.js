f7f98cc1f3dbbda2126ae24120c1a427ad59fcdb
Lucas <Lucaselorenzohenrique@gmail.com>
2022-07-07 12:17:15 -0300

    Modal

---
+++ desempregadosbr/public/js/modal_login.js
@@ -0,0 +1,21 @@
+$(document).on('click', '#login', function(e) {
+    
+    e.preventDefault();
+
+    let modal = $('#exampleModalCenter');
+    let minha_modal = new bootstrap.Modal(modal);
+    minha_modal.show();
+
+
+});
+
+$(document).on('click', '#cadastrar', function(e) {
+    
+    e.preventDefault();
+
+    let modal = $('#exampleModalCenter');
+    let minha_modal = new bootstrap.Modal(modal);
+    minha_modal.show();
+
+
+});
